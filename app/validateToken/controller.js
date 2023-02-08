374ffc00a38000038770fc25bf10cc4a1b841665
Irham Maulana <i.maulana.azn@gmail.com>
2023-02-08 14:18:03 +0700

    changing the validation

--- app/validateToken/controller.js
+++ app/validateToken/controller.js
@@ -3,17 +3,22 @@ const config = require('../../config');
 module.exports = {
     validateToken : async (req,res)=>{
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = await atob(token)
-        try{
-            const decode = jwt.verify(decodedToken, config.jwtKey, (error, decoded)=>{
-                if(decoded){
-                    res.status(200).json({isTokenValid:true,message: 'token valid',user:decoded});
-                }else{
-                    res.status(401).json({isTokenValid:false,message: 'token invalid'});
-                }
-            })
-        }catch(err){
-            res.status(400).json({err});
+        
+        if(token !== 'undefined' && token){
+            const decodedToken = atob(token)
+            try{
+                const decode = jwt.verify(decodedToken, config.jwtKey, (error, decoded)=>{
+                    if(decoded){
+                        res.status(200).json({isTokenValid:true,message: 'token valid',user:decoded});
+                    }else{
+                        res.status(401).json({isTokenValid:false,message: 'token invalid'});
+                    }
+                })
+            }catch(err){
+                res.status(400).json({err});
+            }
+        }else{
+            res.status(401).json({message:'Unauthorized'})
         }
     }
 }
