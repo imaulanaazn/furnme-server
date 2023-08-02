4f192c3fc6d81e01f76e7834b63ef271bca9a135
Irham Maulana <i.maulana.azn@gmail.com>
2023-08-02 15:48:26 +0700

    add getProductById router

--- app/product/router.js
+++ app/product/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
     getAllProducts, 
+    getProductById,
     getTrendingProducts, 
     getRecommendedProducts,
     getNewArrivalProducts, 
@@ -11,11 +12,12 @@ const {
 } = require('./controller')
 
 router.get('/trending', getTrendingProducts);
-router.get('/', getAllProducts);
 router.get('/recommended', getRecommendedProducts);
 router.get('/new', getNewArrivalProducts);
 router.get('/flash-sale', getFlashSaleProducts);
 router.put('/flash-sale', updateFlashSale);
+router.get('/:productId', getProductById);
 router.delete('/:productId/flash-sale', deleteFlashSale);
+router.get('/', getAllProducts);
 
 module.exports = router;
