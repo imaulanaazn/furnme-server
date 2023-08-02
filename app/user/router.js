9902d28ddcda3a6ca7867ea14d70198c3b545919
Irham Maulana <i.maulana.azn@gmail.com>
2023-08-02 15:47:59 +0700

    add updateLikedProducts and getLikedProducts router

--- app/user/router.js
+++ app/user/router.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {authenticateJWT} = require('../utils/controller');
 
-const {addRecentlyViewed, getRecentlyViewed} = require('./controller');
+const {addRecentlyViewed, getRecentlyViewed, updateLikedProducts, getLikedProducts} = require('./controller');
 
 router.post('/validate', authenticateJWT, (req,res)=>{ res.status(200).json({tokenValid: true, ...req.user})})
 router.put('/:userId/recently-viewed', addRecentlyViewed);
 router.get('/:userId/recently-viewed', getRecentlyViewed);
+router.put('/:userId/liked-products', updateLikedProducts);
+router.get('/:userId/liked-products', getLikedProducts);
 
 module.exports = router;
